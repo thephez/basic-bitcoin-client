11131d8d055343da0ba7c18f980b25d6f1c0193b
The Phez <thephez@users.noreply.github.com>
2016-02-19 12:15:23 -0500

    Transaction parsing
    Basic parsing working
    - Not splitting out op codes, etc in input/output scripts

--- transactions.py
+++ transactions.py
@@ -7,6 +7,8 @@ import time
 from cStringIO import StringIO
 import database
 import os
+import sys
+import pprint
 
 logger = logging.getLogger()
 
@@ -54,55 +56,57 @@ class Transactions():
 
         # Loop through each transaction
         for txnum in range(1, txcount + 1):
+            logger.info('Processing Tx # {}'.format(txnum))
 
             tx_in = {}
             tx_out = {}
 
-            logger.info('Tx #: {}'.format(txnum))
-            txinfo['version'] = struct.unpack('<I', decodeData.read(4))[0]
-            txinfo['tx_in_count'] = utility.deserialize_int(decodeData)[0]
+            txdetails['version'] = struct.unpack('<I', decodeData.read(4))[0]
+            txdetails['tx_in_count'] = utility.deserialize_int(decodeData)[0]
             txdetails['Number'] = txnum
 
-            #print(txinfo)
             # Loop through each input
-            for tx in range(txinfo['tx_in_count']):
-                logger.info('Remaining length: {}'.format(txdatalen - decodeData.tell()))
-                txdetails['input_prev_hash_{}'.format(tx)] = hexlify(decodeData.read(32)[::-1])
-                txdetails['input_prev_index'] = struct.unpack('<I', decodeData.read(4))[0]
-                txdetails['input_script_length'] = utility.deserialize_int(decodeData)[0]
-                txdetails['input_script'] = hexlify(decodeData.read(txdetails['input_script_length']))
-                txdetails['input_sequence'] = struct.unpack('<I', decodeData.read(4))[0]
+            logger.info('Tx In Count: {}'.format(txdetails['tx_in_count']))
+            for tx_input in range(txdetails['tx_in_count']):
+                #logger.info('Tx In {}'.format(tx_input))
+                #logger.info('Remaining length: {}'.format(txdatalen - decodeData.tell()))
+                txdetails['input_prev_hash_{}'.format(tx_input)] = hexlify(decodeData.read(32)[::-1])
+                txdetails['input_prev_index_{}'.format(tx_input)] = struct.unpack('<I', decodeData.read(4))[0]
+                txdetails['input_script_length_{}'.format(tx_input)] = utility.deserialize_int(decodeData)[0]
+                txdetails['input_script_{}'.format(tx_input)] = hexlify(decodeData.read(txdetails['input_script_length_{}'.format(tx_input)]))
+                txdetails['input_sequence_{}'.format(tx_input)] = struct.unpack('<I', decodeData.read(4))[0]
 
-                # Parse TX In
-                #logger.info('Hash: {}\t'
-                #      'Index: {}\t'
-                #      'Script Length: {}\t'
-                #      'Script: {}\t'
-                #      'Sequence: {}'.format(txdetails['input_prev_hash_{}'.format(tx)], index, scriptlen, script, seq))
                 #tx_in.append(txdetails)
                 pass
+
+            #logger.info(pprint.pformat(txdetails))
+            #pprint.pprint(txdetails)
 
             txinfo['tx_out_count'] = utility.deserialize_int(decodeData)[0]
             txdetails['tx_out_count'] = txinfo['tx_out_count']
 
             # Loop through each output
-            for tx in range(txinfo['tx_out_count']):
-                txdetails['output_value'] = struct.unpack('<q', decodeData.read(8))[0] # In Satoshis
+            logger.info('Tx Out Count: {}'.format(txinfo['tx_out_count']))
+            for tx_output in range(txinfo['tx_out_count']):
+                #logger.info('Tx Out {}'.format(tx_output))
+                txdetails['output_value_{}'.format(tx_output)] = struct.unpack('<q', decodeData.read(8))[0] # In Satoshis
                 #value = value / 10**8
-                txdetails['output_script_length'] = utility.deserialize_int(decodeData)[0]
-                txdetails['output_script'] = hexlify(decodeData.read(txdetails['output_script_length']))
+                txdetails['output_script_length_{}'.format(tx_output)] = utility.deserialize_int(decodeData)[0]
+                txdetails['output_script_{}'.format(tx_output)] = hexlify(decodeData.read(txdetails['output_script_length_{}'.format(tx_output)]))
 
                 #tx_out.append(txdetails)
-                #logger.info('Value: {}\t'
-                #      'Script Length: {}\t'
-                #      'Script: {}'.format(value, scriptlen, script))
 
-            txinfo['lock_time'] = struct.unpack('<I', decodeData.read(4))[0]
-            txdetails['lock_time'] = txinfo['lock_time']
+            txdetails['lock_time'] = struct.unpack('<I', decodeData.read(4))[0]
+            #txdetails['lock_time'] = txinfo['lock_time']
 
-            print(txinfo)
-            print(txdetails)
-            logger.info('Tx Details: {}'.format(txdetails))
+            #print(txinfo)
+            #print(txdetails)
+            #logger.info('Tx Details: {}'.format(txdetails))
+
+            logger.info(pprint.pformat(txdetails))
+
+            if txnum > 10:
+                sys.exit()
 
         return txinfo
 
