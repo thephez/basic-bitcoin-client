491365d31e26572eea984519ea431b873e40b1df
The Phez <thephez@users.noreply.github.com>
2016-02-19 12:44:07 -0500

    Transaction details
    Append each Tx dict to an overall Tx list
    Cleanup of commented out code, etc.

--- transactions.py
+++ transactions.py
@@ -24,7 +24,7 @@ class Transactions():
         :return:
         '''
 
-        txinfo = {}
+        txinfo = []
         utility = util.Util()
 
         '''
@@ -45,7 +45,6 @@ class Transactions():
         [ 4] LOCK_TIME              <I                                  uint32_t
         '''
 
-        txdetails = {}
         decodeData = StringIO(txdata)
 
         # Get length of data, then reset to start
@@ -56,60 +55,48 @@ class Transactions():
 
         # Loop through each transaction
         for txnum in range(1, txcount + 1):
-            logger.info('Processing Tx # {}'.format(txnum))
-
-            tx_in = {}
-            tx_out = {}
+            txdetails = {}
+            logger.debug('Processing Tx # {}'.format(txnum))
 
             txdetails['version'] = struct.unpack('<I', decodeData.read(4))[0]
             txdetails['tx_in_count'] = utility.deserialize_int(decodeData)[0]
             txdetails['Number'] = txnum
 
             # Loop through each input
-            logger.info('Tx In Count: {}'.format(txdetails['tx_in_count']))
+            logger.debug('Tx In Count: {}'.format(txdetails['tx_in_count']))
             for tx_input in range(txdetails['tx_in_count']):
-                #logger.info('Tx In {}'.format(tx_input))
-                #logger.info('Remaining length: {}'.format(txdatalen - decodeData.tell()))
+                logger.debug('Tx In {}'.format(tx_input))
+                logger.debug('Remaining length: {}'.format(txdatalen - decodeData.tell()))
                 txdetails['input_prev_hash_{}'.format(tx_input)] = hexlify(decodeData.read(32)[::-1])
                 txdetails['input_prev_index_{}'.format(tx_input)] = struct.unpack('<I', decodeData.read(4))[0]
                 txdetails['input_script_length_{}'.format(tx_input)] = utility.deserialize_int(decodeData)[0]
                 txdetails['input_script_{}'.format(tx_input)] = hexlify(decodeData.read(txdetails['input_script_length_{}'.format(tx_input)]))
                 txdetails['input_sequence_{}'.format(tx_input)] = struct.unpack('<I', decodeData.read(4))[0]
 
-                #tx_in.append(txdetails)
-                pass
-
-            #logger.info(pprint.pformat(txdetails))
-            #pprint.pprint(txdetails)
-
-            txinfo['tx_out_count'] = utility.deserialize_int(decodeData)[0]
-            txdetails['tx_out_count'] = txinfo['tx_out_count']
+            txdetails['tx_out_count'] = utility.deserialize_int(decodeData)[0]
 
             # Loop through each output
-            logger.info('Tx Out Count: {}'.format(txinfo['tx_out_count']))
-            for tx_output in range(txinfo['tx_out_count']):
-                #logger.info('Tx Out {}'.format(tx_output))
+            logger.debug('Tx Out Count: {}'.format(txdetails['tx_out_count']))
+            for tx_output in range(txdetails['tx_out_count']):
                 txdetails['output_value_{}'.format(tx_output)] = struct.unpack('<q', decodeData.read(8))[0] # In Satoshis
                 #value = value / 10**8
                 txdetails['output_script_length_{}'.format(tx_output)] = utility.deserialize_int(decodeData)[0]
                 txdetails['output_script_{}'.format(tx_output)] = hexlify(decodeData.read(txdetails['output_script_length_{}'.format(tx_output)]))
 
-                #tx_out.append(txdetails)
-
             txdetails['lock_time'] = struct.unpack('<I', decodeData.read(4))[0]
-            #txdetails['lock_time'] = txinfo['lock_time']
 
-            #print(txinfo)
             #print(txdetails)
-            #logger.info('Tx Details: {}'.format(txdetails))
 
-            logger.info(pprint.pformat(txdetails))
+            #if txnum > 10:
+            #    sys.exit()
 
-            if txnum > 10:
-                sys.exit()
+            txinfo.append(txdetails)
+
+        #logger.info(pprint.pformat(txinfo))
+        pprint.pprint(txinfo[0])
+        pprint.pprint(txinfo[99])
 
         return txinfo
-
 
 
 if __name__ == '__main__':
